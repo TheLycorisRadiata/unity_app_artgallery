b3b60791309cc587dfc9832907da92d8b8fa4422
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2023-01-20 16:29:14 +0100

    User movement: Forward/backward movement and left/right rotation

---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UserInput : MonoBehaviour
+{
+    private static float directionalSpeed, rotationSpeed;
+    private static float horizontalInput, verticalInput;
+
+    void Start()
+    {
+        directionalSpeed = 10f;
+        rotationSpeed = 40f;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0f, 0f, verticalInput) * directionalSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0f, horizontalInput, 0f) * rotationSpeed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 movementVector = ctx.ReadValue<Vector2>();
+        horizontalInput = movementVector.x;
+        verticalInput = movementVector.y;
+    }
+
+    public void OnFire(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+            Debug.Log("Fire!");
+    }
+}
