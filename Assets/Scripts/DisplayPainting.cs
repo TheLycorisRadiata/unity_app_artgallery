9cac6150cb32103c4d4eb35af787a959e412666c
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2023-01-27 16:43:44 +0100

    Resize frame to fit the painting (ongoing)

--- Assets/Scripts/DisplayPainting.cs
+++ Assets/Scripts/DisplayPainting.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -5,19 +6,42 @@ using UnityEngine;
 
 public class DisplayPainting : MonoBehaviour
 {
-    [SerializeField] private int index;
-    private Sprite painting;
-    private PaintingInfo info;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private TextAsset jsonFile;
 
     void Awake()
     {
-        string relativePath = "Sprites/" + index + "/";
-        string absolutePath = Application.dataPath + "/Resources/" + relativePath;
-        string jsonText = "";
+        PaintingInfo info = JsonUtility.FromJson<PaintingInfo>(jsonFile.text);
 
-        painting = Resources.Load<Sprite>(relativePath + "image.jpg");
+        Transform frame = transform.Find("Frame");
+        SpriteRenderer spriteRend = transform.Find("Painting").gameObject.GetComponent<SpriteRenderer>();
 
-        jsonText = File.ReadAllText(absolutePath + "info.json");
-        info = JsonUtility.FromJson<PaintingInfo>(jsonText);
+        // Set sprite
+        spriteRend.sprite = sprite;
+
+        // Scale the sprite down
+        Vector3 desiredScale = new Vector3(1.4f, 1.4f, 1f);
+        spriteRend.transform.localScale = desiredScale;
+
+        // Set frame size
+        float spriteWidth = spriteRend.bounds.size.x;
+        float frameWidth = 1.6f;
+        float diff = spriteWidth - frameWidth;
+        Debug.Log(diff);
+        frame.localScale = new Vector3(frame.localScale.x, frame.localScale.y + diff, frame.localScale.z);
+    }
+}
+
+[System.Serializable]
+public class PaintingInfo
+{
+    public string title, artist, year, description;
+
+    public PaintingInfo(string _title, string _artist, string _year, string _description)
+    {
+        title = _title;
+        artist = _artist;
+        year = _year;
+        description = _description;
     }
 }
