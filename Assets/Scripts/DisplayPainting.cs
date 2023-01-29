3db70cf5045261fcdb585ab0e11dc35ab8cbfb23
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2023-01-29 20:48:03 +0100

    Write info on the painting's label + Method for the painting long description

--- Assets/Scripts/DisplayPainting.cs
+++ Assets/Scripts/DisplayPainting.cs
@@ -3,27 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using TMPro;
 
 public class DisplayPainting : MonoBehaviour
 {
+    // tmp - delete later
+    [SerializeField] private bool displayDescription;
+
+    private static Color activatedDescColor, deactivatedDescColor;
     [SerializeField] private Sprite sprite;
     [SerializeField] private TextAsset jsonFile;
+    private SpriteRenderer spriteRend;
+    private GameObject goDescription;
 
     void Awake()
     {
         PaintingInfo info = JsonUtility.FromJson<PaintingInfo>(jsonFile.text);
+        Transform infoCanvas = transform.Find("Canvas");
+        TextMeshProUGUI labelText, descriptionText;
+
+        float spriteWidth, spriteFactor, frameWidth;
 
         Transform frame = transform.Find("Frame");
-        SpriteRenderer spriteRend = transform.Find("Painting").gameObject.GetComponent<SpriteRenderer>();
+        spriteRend = transform.Find("Painting").gameObject.GetComponent<SpriteRenderer>();
 
         // Set sprite
         spriteRend.sprite = sprite;
 
         // Set frame width
-        float spriteWidth = Math.Max(spriteRend.bounds.size.x, spriteRend.bounds.size.z);
-        float spriteFactor = 0.0145f;
-        float frameWidth = spriteWidth / spriteFactor;
+        spriteWidth = Math.Max(spriteRend.bounds.size.x, spriteRend.bounds.size.z);
+        spriteFactor = 0.0145f;
+        frameWidth = spriteWidth / spriteFactor;
         frame.localScale = new Vector3(frame.localScale.x, frameWidth, frame.localScale.z);
+
+        // Set label text
+        labelText = infoCanvas.Find("Label").GetComponent<TextMeshProUGUI>();
+        labelText.text = "\"" + info.title + "\"\n" + info.artist + "\n" + info.year;
+
+        // Set description text
+        goDescription = infoCanvas.Find("Description").gameObject;
+        descriptionText = goDescription.GetComponent<TextMeshProUGUI>();
+        descriptionText.text = info.description;
+        // Set the colors
+        activatedDescColor = new Color(0.11f, 0.09f, 0.09f, 1f); // #1C1717
+        deactivatedDescColor = new Color(1f, 1f, 1f, 1f); // #FFF
+        // Deactivate it
+        DescriptionActivation(false);
+    }
+
+    void LateUpdate()
+    {
+        DescriptionActivation(displayDescription);
+    }
+
+    private void DescriptionActivation(bool activate)
+    {
+        if (activate)
+        {
+            spriteRend.color = activatedDescColor;
+            goDescription.SetActive(true);
+        }
+        else
+        {
+            spriteRend.color = deactivatedDescColor;
+            goDescription.SetActive(false);
+        }
     }
 }
 
