f6e6c682a892b33c3aa11c350ac45dd4627df35b
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2023-02-13 10:42:02 +0100

    Add code to fetch from an API (unused)

--- Assets/Scripts/DisplayPainting.cs
+++ Assets/Scripts/DisplayPainting.cs
@@ -93,17 +93,3 @@ public class DisplayPainting : MonoBehaviour
         cursorAppearance.ChangeCursorAppearance(CursorAppearance.CursorState.DEFAULT);
     }
 }
-
-[System.Serializable]
-public class PaintingInfo
-{
-    public string title, artist, year, description;
-
-    public PaintingInfo(string _title, string _artist, string _year, string _description)
-    {
-        title = _title;
-        artist = _artist;
-        year = _year;
-        description = _description;
-    }
-}
